787d8de335ebde15c786d5b161a72fde845a003e
MorseWayne <2539720515@qq.com>
2023-05-26 11:28:45 +0800

    Update xx_hash_test.c
    1. 优化static变量
    2. 优化state初始化函数
    3. 优化内联标志
    4. 增加cache机制

--- xx_hash_test.c
+++ xx_hash_test.c
@@ -2,6 +2,7 @@
 #include "sgl.h" /* 包含了 SGL 结构体的定义以及 SGL_HashValue 的声明。 */
 #include <stdbool.h>
 #include <arm_neon.h>
+#include <string.h>
 
 #define XXH64_LANE_COUNT ((size_t)4)
 #define XXH64_SEED       ((uint64_t)0)
@@ -12,6 +13,8 @@
 #define XXH64_PRIME_3 ((uint64_t)0x165667B19E3779F9ULL)
 #define XXH64_PRIME_4 ((uint64_t)0x85EBCA77C2B2AE63ULL)
 #define XXH64_PRIME_5 ((uint64_t)0x27D4EB2F165667C5ULL)
+
+#define ROTATE_LEFT(value, shift) (value << shift) | (value >> (64 - shift))
 
 /*
  * 用于保存 XXH64 算法的状态。
@@ -24,61 +27,19 @@ typedef struct {
     bool isLarge;                               /* 状态是否已经接受了大于 32 字节的数据量。 */
 } XXH64State;
 
-static uint64_t inline XXH64_Read64(const uint8_t *data)
-{
-    return *(const uint64_t *)data;
-}
-
-/*
- * 初始化 XXH64 算法的状态。
- */
-static void XXH64_StateInit(XXH64State *state)
-{
-    static const uint64_t INIT_ACC[] = {
-        XXH64_SEED + XXH64_PRIME_1 + XXH64_PRIME_2,
-        XXH64_SEED + XXH64_PRIME_2,
-        XXH64_SEED,
-        XXH64_SEED - XXH64_PRIME_1,
-    };
-
-    state->bufferSize = 0;
-    state->totalSize = 0;
-    state->isLarge = false;
-    for (size_t i = 0; i < XXH64_LANE_COUNT; i++) {
-        state->acc[i] = INIT_ACC[i];
-    }
-}
-
-/*
- * 返回 x 循环左移 n 位的结果。
- */
-static inline uint64_t XXH64_RotateLeft(uint64_t value, uint8_t shift)
-{
-    __asm__("ROR %[value], %[value], %[shift]"
-            : [value] "+r"(value)
-            : [shift] "r"((unsigned int)(33)));
-    return value;
-}
-
 static inline uint64_t XXH64_Round(uint64_t accn, uint64_t lane)
 {
     // 步骤1
     accn += XXH64_PRIME_2 * lane;
 
-    // 步骤2: 循环左移31位
-    // uint64x2_t v = vld1q_u64(&accn);
-    // uint64x2_t result = vorrq_u64(vshlq_n_u64(v, 31), vshrq_n_u64(v, 33));
-    // vst1q_lane_u64(&accn, result, 0);
-
-    // accn = (accn << 31) | (accn >> 33);
-    __asm__("ROR %[accn], %[accn], %[shift]" : [accn] "+r"(accn) : [shift] "r"((unsigned int)(33)));
+    accn = (accn << 31) | (accn >> 33);
 
     // 步骤3
     accn *= XXH64_PRIME_1;
     return accn;
 }
 
-static uint64_t XXH64_MergeAccumulator(uint64_t finalAcc, uint64_t accn)
+static inline uint64_t XXH64_MergeAccumulator(uint64_t finalAcc, uint64_t accn)
 {
     uint64_t tmp = finalAcc;
     tmp = tmp ^ XXH64_Round(0, accn);
@@ -90,15 +51,13 @@ static uint64_t XXH64_MergeAccumulator(uint64_t finalAcc, uint64_t accn)
 /*
  * 如果总数据量小于 32 字节，则返回一个特殊值作为 finalAcc；否则将 4 个累加器整合为 finalAcc。
  */
-static uint64_t XXH64_FinalAcc(XXH64State *state)
+static inline uint64_t XXH64_FinalAcc(XXH64State *state)
 {
-    static const int ROTATE_LEFT[] = { 1, 7, 12, 18 };
+    register uint64_t finalAcc = ROTATE_LEFT(state->acc[0], 1);
+    finalAcc += ROTATE_LEFT(state->acc[1], 7);
+    finalAcc += ROTATE_LEFT(state->acc[2], 12);
+    finalAcc += ROTATE_LEFT(state->acc[3], 18);
 
-    uint64_t finalAcc = 0;
-
-    for (size_t i = 0; i < XXH64_LANE_COUNT; i++) {
-        finalAcc += XXH64_RotateLeft(state->acc[i], ROTATE_LEFT[i]);
-    }
     for (size_t i = 0; i < XXH64_LANE_COUNT; i++) {
         finalAcc = XXH64_MergeAccumulator(finalAcc, state->acc[i]);
     }
@@ -108,11 +67,11 @@ static uint64_t XXH64_FinalAcc(XXH64State *state)
 }
 
 /* 对 finalAcc 进行雪崩操作、使得输入发生微小变动时，输出的每一位都有机会被翻转。 */
-static uint64_t XXH64_Mix(uint64_t finalAcc)
+static inline uint64_t XXH64_Mix(uint64_t finalAcc)
 {
-    static const int MIX_COUNT = 3;
-    static const int MIX_SHIFT[] = { 33, 29, 32 };
-    static const uint64_t MIX_MUL[] = { XXH64_PRIME_2, XXH64_PRIME_3, 1 };
+    const int MIX_COUNT = 3;
+    const int MIX_SHIFT[] = { 33, 29, 32 };
+    const uint64_t MIX_MUL[] = { XXH64_PRIME_2, XXH64_PRIME_3, 1 };
     uint64_t tmp = finalAcc;
     for (size_t i = 0; i < MIX_COUNT; i++) {
         tmp ^= tmp >> MIX_SHIFT[i];
@@ -124,24 +83,52 @@ static uint64_t XXH64_Mix(uint64_t finalAcc)
 /*
  * 将当前状态整合为哈希值返回。
  */
-static uint64_t XXH64_Digest(XXH64State *state)
+static inline uint64_t XXH64_Digest(XXH64State *state)
 {
     uint64_t finalAcc = XXH64_FinalAcc(state);
+    // finalAcc = XXH64_SolveRemaining(state, finalAcc);
     finalAcc = XXH64_Mix(finalAcc);
     return finalAcc;
 }
 
+#define Multi(input, output, step)                  \
+    if (input != lastinput##step) {                 \
+        output = (uint64_t)(input * XXH64_PRIME_2); \
+        lastinput##step = input;                    \
+        lastoutput##step = output;                  \
+    } else {                                        \
+        output = lastoutput##step;                  \
+    }
+
 uint64_t SGL_HashValue(SGL *sgl)
 {
     XXH64State state = {};
-    XXH64_StateInit(&state);
-    register uint64_t num1 = state.acc[0];
-    register uint64_t num2 = state.acc[1];
-    register uint64_t num3 = state.acc[2];
-    register uint64_t num4 = state.acc[3];
+   // XXH64_StateInit(&state);
+    // memset(&state,0,sizeof(state));
+    register uint64_t num1 = 0x60ea27eeadc0b5d6;
+    register uint64_t num2 = 0xc2b2ae3d27d4eb4f;
+    register uint64_t num3 = 0;
+    register uint64_t num4 = 0x61c8864e7a143579;
 
-    const uint64_t shift = 31;
-    const uint64_t shiftRight = 31;
+
+    uint64_t product = 0;
+    uint64_t lastinput0 = 0;
+    uint64_t lastoutput0 = 0;
+    uint64_t lastinput1 = 0;
+    uint64_t lastoutput1 = 0;
+    uint64_t lastinput2 = 0;
+    uint64_t lastoutput2 = 0;
+    uint64_t lastinput3 = 0;
+    uint64_t lastoutput3 = 0;
+    (void)(lastinput0);
+    (void)(lastinput1);
+    (void)(lastinput2);
+    (void)(lastinput3);
+    (void)(lastoutput0);
+    (void)(lastoutput1);
+    (void)(lastoutput2);
+    (void)(lastoutput3);
+
 
     state.totalSize = sgl->entryCount * 8192;
     for (size_t i = 0; i < sgl->entryCount; i++) {
@@ -153,40 +140,41 @@ uint64_t SGL_HashValue(SGL *sgl)
             // XXH64_Update_new(&state, entrybuf + offset, BYTE_PER_SECTOR_NOPI);
             const uint64_t *data64 = (uint64_t *)(entrybuf + offset);
             for (size_t i = 0; i < 8; ++i) {
+                Multi(data64[0], product, 0);
+                num1 += product;
+                num1 = (uint64_t)((num1 << 31) | (num1 >> 33)) * XXH64_PRIME_1;
+                // num1 += (uint64_t)(data64[0] * XXH64_PRIME_2);
+                // __asm__("ROR %[accn], %[accn], %[shift]" : [accn] "+r"(num1) : [shift] "r"((unsigned int)(33)));
+                // num1 *= XXH64_PRIME_1;
 
-                num1 += (uint64_t)(data64[0] * XXH64_PRIME_2);
-                __asm__("ROR %[num1], %[num1], %[shift]" : [num1] "+r"(num1) : [shift] "r"(shiftRight)); 
-                num1 *= XXH64_PRIME_1;
 
-                num2 += (uint64_t)(data64[1] * XXH64_PRIME_2);
-                __asm__("ROR %[num2], %[num2], %[shift]" : [num2] "+r"(num2) : [shift] "r"(shiftRight)); 
-                num2 *= XXH64_PRIME_1;
+                Multi(data64[4], product, 1);
+                num1 += product;
+                num1 = (uint64_t)((num1 << 31) | (num1 >> 33)) * XXH64_PRIME_1;
+
+                Multi(data64[1], product, 2);
+                num2 += product;
+                // num2 += (uint64_t)(data64[1] * XXH64_PRIME_2);
+                num2 = (uint64_t)((num2 << 31) | (num2 >> 33)) * XXH64_PRIME_1;
+
+                Multi(data64[5], product, 3);
+                num2 += product;
+                // num2 += (uint64_t)(data64[5] * XXH64_PRIME_2);
+                num2 = (uint64_t)((num2 << 31) | (num2 >> 33)) * XXH64_PRIME_1;
+
 
                 num3 += (uint64_t)(data64[2] * XXH64_PRIME_2);
-                __asm__("ROR %[num3], %[num3], %[shift]" : [num3] "+r"(num3) : [shift] "r"(shiftRight)); 
-                num3 *= XXH64_PRIME_1;
-
-                num4 += (uint64_t)(data64[3] * XXH64_PRIME_2);
-                __asm__("ROR %[num4], %[num4], %[shift]" : [num4] "+r"(num4) : [shift] "r"(shiftRight)); 
-                num4 *= XXH64_PRIME_1;
-
-                num1 += (uint64_t)(data64[4] * XXH64_PRIME_2);
-                __asm__("ROR %[num1], %[num1], %[shift]" : [num1] "+r"(num1) : [shift] "r"(shiftRight)); 
-                num1 *= XXH64_PRIME_1;
-
-                num2 += (uint64_t)(data64[5] * XXH64_PRIME_2);
-                __asm__("ROR %[num2], %[num2], %[shift]" : [num2] "+r"(num2) : [shift] "r"(shiftRight)); 
-                num2 *= XXH64_PRIME_1;
+                num3 = (uint64_t)((num3 << 31) | (num3 >> 33)) * XXH64_PRIME_1;
 
                 num3 += (uint64_t)(data64[6] * XXH64_PRIME_2);
-                __asm__("ROR %[num3], %[num3], %[shift]" : [num3] "+r"(num3) : [shift] "r"(shiftRight)); 
-                num3 *= XXH64_PRIME_1;
+                num3 = (uint64_t)((num3 << 31) | (num3 >> 33)) * XXH64_PRIME_1;
+
+                num4 += (uint64_t)(data64[3] * XXH64_PRIME_2);
+                num4 = (uint64_t)((num4 << 31) | (num4 >> 33)) * XXH64_PRIME_1;
 
                 num4 += (uint64_t)(data64[7] * XXH64_PRIME_2);
-                __asm__("ROR %[num4], %[num4], %[shift]" : [num4] "+r"(num4) : [shift] "r"(shiftRight)); 
-                num4 *= XXH64_PRIME_1;
+                num4 = (uint64_t)((num4 << 31) | (num4 >> 33)) * XXH64_PRIME_1;
 
-    
                 data64 += 8;
             }
         }
@@ -244,7 +232,6 @@ uint64_t multiply(uint32_t low1, uint32_t high1, uint32_t low2, uint32_t high2)
 
     return vget_lane_u64(sum, 0);
 }
-
 
 uint64_t SGL_HashValue_Raw(SGL *sgl)
 {
@@ -310,4 +297,12 @@ int main()
     uint64_t a = UINT64_MAX - 1;
     uint64_t b = UINT64_MAX - 1000;
 
+
+    register uint64_t num1 = XXH64_PRIME_1 + XXH64_PRIME_2;
+    register uint64_t num2 = XXH64_PRIME_2;
+    register uint64_t num3 = 0;
+    register uint64_t num4 = 0 - XXH64_PRIME_1;
+    cout << hex << num1 << endl;
+    cout << hex << num2 << endl;
+    cout << hex << num4 << endl;
 }
